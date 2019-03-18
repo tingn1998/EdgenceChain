84a8a8e3e6ac49501f59e38b4d9765651d35f39e
Jin <jlxufly@gmail.com>
2019-03-18 18:24:14 +0800

    domain name to ip, and own public ip

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ from EdgenceChain import EdgenceChain
 
 if __name__ == '__main__':
 
-    #from p2p.Peer import Peer
-    #peer = Peer('https://baidu.com',80)
-    #print(peer)
     edgencechain = EdgenceChain()
     edgencechain.start()
