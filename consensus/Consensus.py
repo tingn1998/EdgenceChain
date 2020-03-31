21fd2504caab20ee189c005e07ff91d3de62a6a2
piaoliangkb <piaoliangkb@gmail.com>
2020-03-31 23:28:57 +0800

    Code format

--- consensus/Consensus.py
+++ consensus/Consensus.py
@@ -9,25 +9,28 @@ from typing import Union
 
 
 logging.basicConfig(
-    level=getattr(logging, os.environ.get('TC_LOG_LEVEL', 'INFO')),
-    format='[%(asctime)s][%(module)s:%(lineno)d] %(levelname)s %(message)s')
+    level=getattr(logging, os.environ.get("TC_LOG_LEVEL", "INFO")),
+    format="[%(asctime)s][%(module)s:%(lineno)d] %(levelname)s %(message)s",
+)
 logger = logging.getLogger(__name__)
 
-class PoW(object):
 
+class PoW(object):
     @classmethod
-    def mine(cls, block: Block, mine_interrupt: threading.Event = None) -> Union[Block, None]:
+    def mine(
+        cls, block: Block, mine_interrupt: threading.Event = None
+    ) -> Union[Block, None]:
         start = time.time()
         nonce = 0
-        target = (1 << (256 - block.bits))
+        target = 1 << (256 - block.bits)
         mine_interrupt.clear()
 
-        logger.info(f'[consensus] mining after block {block.prev_block_hash}')
+        logger.info(f"[consensus] mining after block {block.prev_block_hash}")
         while int(Utils.sha256d(block.header(nonce)), 16) >= target:
             nonce += 1
 
             if mine_interrupt.is_set():
-                logger.info(f'[consensus] mining interrupted +++ {nonce}')
+                logger.info(f"[consensus] mining interrupted +++ {nonce}")
                 mine_interrupt.clear()
                 return None
 
@@ -35,6 +38,7 @@ class PoW(object):
         duration = max(time.time() - start, 0.0001)
         khs = (block.nonce // duration) // 1000
         logger.info(
-            f'[consensus] mining block found at nonce={nonce} using {round(duration, 4)} s at rate {khs} KH/s: {block.id}')
+            f"[consensus] mining block found at nonce={nonce} using {round(duration, 4)} s at rate {khs} KH/s: {block.id}"
+        )
 
         return block
--- utils/Utils.py
+++ utils/Utils.py
@@ -10,27 +10,34 @@ import random
 import os
 from functools import lru_cache, wraps
 from typing import (
-    Iterable, NamedTuple, Dict, Mapping, Union, get_type_hints, Tuple,
-    Callable)
+    Iterable,
+    NamedTuple,
+    Dict,
+    Mapping,
+    Union,
+    get_type_hints,
+    Tuple,
+    Callable,
+)
 from _thread import RLock
 
 from params.Params import Params
 
 logging.basicConfig(
-    level=getattr(logging, os.environ.get('TC_LOG_LEVEL', 'INFO')),
-    format='[%(asctime)s][%(module)s:%(lineno)d] %(levelname)s %(message)s')
+    level=getattr(logging, os.environ.get("TC_LOG_LEVEL", "INFO")),
+    format="[%(asctime)s][%(module)s:%(lineno)d] %(levelname)s %(message)s",
+)
 logger = logging.getLogger(__name__)
 
 
-
 class Utils(object):
-
     @classmethod
     def serialize(cls, obj) -> str:
         """NamedTuple-flavored serialization to JSON."""
+
         def contents_to_primitive(o):
-            if hasattr(o, '_asdict'):
-                o = {**o._asdict(), '_type': type(o).__name__}
+            if hasattr(o, "_asdict"):
+                o = {**o._asdict(), "_type": type(o).__name__}
             elif isinstance(o, (list, tuple)):
                 return [contents_to_primitive(i) for i in o]
             elif isinstance(o, bytes):
@@ -41,7 +48,10 @@ class Utils(object):
                 for k, v in o.items():
                     o[k] = contents_to_primitive(v)
             return o
-        return json.dumps(contents_to_primitive(obj), sort_keys=True, separators=(',', ':'))
+
+        return json.dumps(
+            contents_to_primitive(obj), sort_keys=True, separators=(",", ":")
+        )
 
     @classmethod
     def deserialize(cls, serialized: str, gs: dict) -> object:
@@ -53,9 +63,8 @@ class Utils(object):
             elif not isinstance(o, Mapping):
                 return o
 
-            _type = gs[o.pop('_type', None)]
-            bytes_keys = {
-                k for k, v in get_type_hints(_type).items() if v == bytes}
+            _type = gs[o.pop("_type", None)]
+            bytes_keys = {k for k, v in get_type_hints(_type).items() if v == bytes}
 
             for k, v in o.items():
                 o[k] = contents_to_objs(v)
@@ -64,6 +73,7 @@ class Utils(object):
                     o[k] = binascii.unhexlify(o[k]) if o[k] else o[k]
 
             return _type(**o)
+
         return contents_to_objs(json.loads(serialized))
 
     @classmethod
@@ -77,10 +87,12 @@ class Utils(object):
     @classmethod
     def encode_chain_data(cls, chain: Iterable[NamedTuple]) -> bytes:
         """Our protocol is: first 4 bytes signify msg length."""
+
         def int_to_20bytes(a: int) -> bytes:
             int_str = str(a)
-            int_str = '0'*(20 - len(int_str)) + int_str
+            int_str = "0" * (20 - len(int_str)) + int_str
             return int_str.encode()
+
         block_len = len(chain)
         to_send = cls.serialize(chain).encode()
         msg_len = len(to_send)
@@ -89,8 +101,10 @@ class Utils(object):
     @classmethod
     def encode_socket_data(cls, data: object) -> bytes:
         """Our protocol is: first 4 bytes signify msg length."""
+
         def int_to_8bytes(a: int) -> bytes:
             return binascii.unhexlify(f"{a:0{8}x}")
+
         to_send = Utils.serialize(data).encode()
         return int_to_8bytes(len(to_send)) + to_send
 
@@ -103,23 +117,29 @@ class Utils(object):
         tries_left = int(Params.TRIES_MAXIMUM)
 
         if tries_left <= 0:
-            logger.info(f'[utils] initial tries_left is less than 1, will not send data to other nodes')
+            logger.info(
+                f"[utils] initial tries_left is less than 1, will not send data to other nodes"
+            )
             return -2
 
         while tries_left > 0:
 
             try:
                 if not itself:
-                    #logger.info(f'[utils] begin to create socket connection with peer {peer}' )
+                    # logger.info(f'[utils] begin to create socket connection with peer {peer}' )
                     pass
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #socket.create_connection(peer(), timeout=30) as s:
+                with socket.socket(
+                    socket.AF_INET, socket.SOCK_STREAM
+                ) as s:  # socket.create_connection(peer(), timeout=30) as s:
                     s.connect(peer())
-                    #logger.info(f'[utils] succeed to create socket connection with {peer}')
+                    # logger.info(f'[utils] succeed to create socket connection with {peer}')
                     s.sendall(cls.encode_socket_data(data))
-                    #logger.info(f'[utils] succeed to send data to {peer}')
+                    # logger.info(f'[utils] succeed to send data to {peer}')
             except ConnectionRefusedError as e:
-                logger.exception(f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
-                                 f' in {Params.TRIES_MAXIMUM+1-tries_left}th time')
+                logger.exception(
+                    f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
+                    f" in {Params.TRIES_MAXIMUM+1-tries_left}th time"
+                )
                 try:
                     s.close()
                 except:
@@ -129,8 +149,10 @@ class Utils(object):
                 if tries_left <= 0:
                     return 1
             except TimeoutError as e:
-                logger.exception(f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
-                                 f' in {Params.TRIES_MAXIMUM+1-tries_left}th time')
+                logger.exception(
+                    f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
+                    f" in {Params.TRIES_MAXIMUM+1-tries_left}th time"
+                )
                 try:
                     s.close()
                 except:
@@ -140,8 +162,10 @@ class Utils(object):
                 if tries_left <= 0:
                     return 2
             except BrokenPipeError as e:
-                logger.exception(f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
-                                 f' in {Params.TRIES_MAXIMUM+1-tries_left}th time')
+                logger.exception(
+                    f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
+                    f" in {Params.TRIES_MAXIMUM+1-tries_left}th time"
+                )
                 try:
                     s.close()
                 except:
@@ -151,8 +175,10 @@ class Utils(object):
                 if tries_left <= 0:
                     return 3
             except Exception as e:
-                logger.exception(f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
-                                 f' in {Params.TRIES_MAXIMUM+1-tries_left}th time')
+                logger.exception(
+                    f'[utils] {repr(e)}, failed to send to {peer} data about {Actions.num2name[str(data.action)] if hasattr(data, "action") else None} '
+                    f" in {Params.TRIES_MAXIMUM+1-tries_left}th time"
+                )
                 try:
                     s.close()
                 except:
@@ -168,9 +194,12 @@ class Utils(object):
                     pass
                 if not itself:
                     if hasattr(data, "action") and data.action != 10:
-                        logger.info(f'[utils] succeed in sending to {peer} data about {Actions.num2name[str(data.action)]}'
-                                f' in {Params.TRIES_MAXIMUM+1-tries_left}th time')
+                        logger.info(
+                            f"[utils] succeed in sending to {peer} data about {Actions.num2name[str(data.action)]}"
+                            f" in {Params.TRIES_MAXIMUM+1-tries_left}th time"
+                        )
                 return 0
+
     @classmethod
     def is_peer_valid(cls, peer) -> bool:
         try:
@@ -199,12 +228,11 @@ class Utils(object):
             return True
         return True
 
-
     @classmethod
     def read_all_from_socket(cls, req, gs) -> object:
-        data = b''
+        data = b""
         # Our protocol is: first 4 bytes signify msg length.
-        msg_len = int(binascii.hexlify(req.recv(4) or b'\x00'), 16)
+        msg_len = int(binascii.hexlify(req.recv(4) or b"\x00"), 16)
         logger.info(f"[utils] message length is {msg_len}")
 
         while msg_len > 0:
@@ -220,6 +248,3 @@ class Utils(object):
             msg_len -= len(tdat)
 
         return cls.deserialize(data.decode(), gs) if data else None
-
-
-
