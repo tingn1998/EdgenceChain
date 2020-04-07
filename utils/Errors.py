9403b7d788f3720f94111a86173a085412f8b91d
piaoliangkb <piaoliangkb@gmail.com>
2020-04-07 23:07:25 +0800

    Code format

--- utils/Errors.py
+++ utils/Errors.py
@@ -1,23 +1,23 @@
-
-
 class BaseException(Exception):
     def __init__(self, msg):
         self.msg = msg
 
+
 class TxUnlockError(BaseException):
     pass
 
-class ChainFileLostError (BaseException):
+
+class ChainFileLostError(BaseException):
     pass
 
-class TxnValidationError(BaseException):
-    def __init__(self, *args, to_orphan = None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.to_orphan = to_orphan
 
-class BlockValidationError(BaseException):
+class TxnValidationError(BaseException):
     def __init__(self, *args, to_orphan=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.to_orphan = to_orphan
 
 
+class BlockValidationError(BaseException):
+    def __init__(self, *args, to_orphan=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.to_orphan = to_orphan
